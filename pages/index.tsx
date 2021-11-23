bbfd563e86c9759301a28ab3ed75e1e1463786ad
Max Hoffmann <praxis.max@gmail.com>
2021-11-23 18:33:33 +0100

    changed db provider + first read/write with db

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,27 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import {PrismaClient} from '@prisma/client'
 
-const prisma = new PrismaClient()
 
+  export const getServerSideProps: GetServerSideProps = async (context) => {
+    const prisma = new PrismaClient()
+    const products  = await prisma.product.findMany()
 
+    // Creating a new record
+    await prisma.product.create({
+      data: {
+        barcode: 'test',
+        price: 1
+      }
+    });
+
+    return {
+    props : { products }
+    }
+  }
     
 
 
-const Home: NextPage = () => {
+const Home: NextPage = ({products}) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -23,41 +37,12 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        {products.map((product) => (
+          <p>{product.createdAt.toISOString()}</p>
+        )
+            )}
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      
       </main>
 
       <footer className={styles.footer}>
